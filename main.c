b396077f806333c23cee56a8ae70c92758727bc0
Pola-Gerges-Younan <gergespola747@gmail.com>
2020-09-28 03:31:27 +0200

    C file

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+   int arr [2][2] = {1,2,3,4};
+
+   int (*p)[2] = arr;
+
+   for (int i = 0 ; i < 2 ; i++)
+   {
+       printf("Raw number %d has numbers : ",i+1);
+       for (int j= 0 ; j < 2 ; j++)
+       {
+           printf("%d ",p[i][j]);
+       }
+       printf("\n");
+   }
+
+    return 0 ;
+}
+
